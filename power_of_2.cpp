e17e573bb67a3835e97bd33e1deb32e11b6cd848
priyanshu-mishra01 <priyanshumishra223@gmail.com>
2023-01-29 19:02:12 +0530

    updating power_of_2.cpp

--- power_of_2.cpp
+++ power_of_2.cpp
@@ -1,1 +1,21 @@
-#include<io
+#include<bits/stdc++.h>
+#define int long long int
+using namespace std;
+
+int power(int n)
+{
+	int res = 1, base = 2, mod = (int)1e9 + 7;
+        while (n > 0) {
+            if (n % 2 == 1)
+                res = res * base % mod;
+            base = base * base % mod;
+            n >>= 1;
+        }
+    return res;
+}
+int32_t main()
+{
+    int n = 10000;
+    cout<<power(n)<<endl;
+    return 0;
+}
